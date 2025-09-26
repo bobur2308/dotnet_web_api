27393cf60784b099dc08b8605d3be7b01aba3df0
Bobur Niyozov <76684466+bobur2308@users.noreply.github.com>
2025-09-26 18:42:32 +0500

    updates

--- webapp/Data/ApplicationDbContext.cs
+++ webapp/Data/ApplicationDbContext.cs
@@ -4,7 +4,7 @@ using webapp.Models;
 
 namespace webapp.Data
 {
-    public class ApplicationDbContext : IdentityDbContext<AppUser>
+    public class ApplicationDbContext : DbContext
     {
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> dbContextOptions) : base(dbContextOptions)
         {
